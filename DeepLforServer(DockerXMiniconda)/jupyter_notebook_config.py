e28aef4008856dd5e20077ae58de9e0025594f74
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-09 19:06:10 +0700

    tes12

--- DeepLforServer(DockerXMiniconda)/jupyter_notebook_config.py
+++ DeepLforServer(DockerXMiniconda)/jupyter_notebook_config.py
@@ -4,6 +4,5 @@ from IPython.lib import passwd
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = int(os.getenv('PORT', 8888))
 c.NotebookApp.open_browser = False
-c.MultiKernelManager.default_kernel_name = 'python2'
-c.NotebookApp.kernel_spec_manager_class = 'environment_kernels.EnvironmentKernelSpecManager'
+c.MultiKernelManager.default_kernel_name = 'python3'
 c.NotebookApp.password = u'sha1:035a13e895a5:8a3398f1576a32cf938f9236db03f5e8668356c5'

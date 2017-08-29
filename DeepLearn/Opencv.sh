b5e57590db1c54d303017f67c88ccf73c52ed275
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-29 14:15:54 +0700

    sh prob

--- DeepLearn/Opencv.sh
+++ DeepLearn/Opencv.sh
@@ -3,8 +3,19 @@ unzip 3.2.0.zip
 cd opencv-3.2.0/
 mkdir build
 cd build/    
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D FORCE_VTK=ON -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUBLAS=ON -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" -D WITH_GDAL=ON -D WITH_XINE=ON -D BUILD_EXAMPLES=ON ..
-make -j $(($(nproc) + 1))
+cmake -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX=/usr/local \
+    -DFORCE_VTK=ON \
+    -DWITH_TBB=ON \
+    -DWITH_V4L=ON \
+    -DWITH_QT=ON \
+    -DWITH_OPENGL=ON \
+    -DWITH_CUBLAS=ON \
+    -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
+    -DWITH_GDAL=ON \
+    -DWITH_XINE=ON \
+    -DBUILD_EXAMPLES=ON ..
+make -j4
 make install
 /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 ldconfig

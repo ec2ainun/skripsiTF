9065c012082d0582d543892244b713c8ed460f5c
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-29 12:13:33 +0700

    build opncv

---
+++ DeepLearn/Opencv.sh
@@ -0,0 +1,7 @@
+wget https://github.com/opencv/opencv/archive/3.2.0.zip
+unzip opencv-3.2.0.zip
+cd opencv-3.2.0/
+mkdir build
+cd build/    
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D FORCE_VTK=ON -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUBLAS=ON -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" -D WITH_GDAL=ON -D WITH_XINE=ON -D BUILD_EXAMPLES=ON ..
+make -j $(($(nproc) + 1))

3aba8c499599a25ae47629eca43c17fb0e4e65d6
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-29 13:55:57 +0700

    sh problem

--- DeepLearn/Opencv.sh
+++ DeepLearn/Opencv.sh
@@ -5,3 +5,7 @@ mkdir build
 cd build/    
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D FORCE_VTK=ON -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_CUBLAS=ON -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" -D WITH_GDAL=ON -D WITH_XINE=ON -D BUILD_EXAMPLES=ON ..
 make -j $(($(nproc) + 1))
+make install
+/bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
+apt-get update

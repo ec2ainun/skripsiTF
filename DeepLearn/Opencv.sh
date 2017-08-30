8d528413d86d0e9cbe368b10a14c36d5076791ff
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-30 12:47:13 +0700

    cudnn5

--- DeepLearn/Opencv.sh
+++ DeepLearn/Opencv.sh
@@ -7,20 +7,20 @@ cd ~/opencv/
 git checkout 3.3.0
 mkdir build
 cd build/    
-cmake -DCMAKE_BUILD_TYPE=RELEASE \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DFORCE_VTK=ON \
-    -DWITH_TBB=ON \
-    -DWITH_V4L=ON \
-    -DWITH_QT=ON \
-    -DWITH_OPENGL=ON \
-    -DWITH_CUBLAS=ON \
-    -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
-    -DWITH_GDAL=ON \
-    -DWITH_XINE=ON \
-    -DCUDA_CUDA_LIBRARY=/usr/local/cuda-8.0/targets/x86_64-linux/lib/stubs/libcuda.so \
-    -DOPENCV_EXTRA_MODULES_PATH= ~/opencv_contrib/modules \
-    -DBUILD_EXAMPLES=ON ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D FORCE_VTK=ON \
+    -D WITH_TBB=ON \
+    -D WITH_V4L=ON \
+    -D WITH_QT=ON \
+    -D WITH_OPENGL=ON \
+    -D WITH_CUBLAS=ON \
+    -D CUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
+    -D WITH_GDAL=ON \
+    -D WITH_XINE=ON \
+    -D CUDA_CUDA_LIBRARY=/usr/local/cuda-8.0/targets/x86_64-linux/lib/stubs/libcuda.so \
+    -D OPENCV_EXTRA_MODULES_PATH= ~/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
 make -j $(($(nproc) + 1))
 make install
 /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'

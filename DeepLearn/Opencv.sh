cdad234b1673dacfb9c6064b85b307b76f943ce2
Moch. Ainun Najib <ec2ainun@gmail.com>
2017-08-30 18:40:00 +0700

    cmake

--- DeepLearn/Opencv.sh
+++ DeepLearn/Opencv.sh
@@ -4,21 +4,7 @@ git clone --branch 3.3.0 --depth 1 https://github.com/opencv/opencv_contrib.git
 cd opencv
 mkdir build
 cd build    
-cmake \ 
-    -DCMAKE_BUILD_TYPE=RELEASE \
-    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ \
-    -DCMAKE_INSTALL_PREFIX=/usr/local \
-    -DFORCE_VTK=ON \
-    -DWITH_TBB=ON \
-    -DWITH_V4L=ON \
-    -DWITH_QT=ON \
-    -DWITH_OPENGL=ON \
-    -DWITH_CUBLAS=ON \
-    -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" \
-    -DWITH_GDAL=ON \
-    -DWITH_XINE=ON \
-    -DCUDA_CUDA_LIBRARY=/usr/local/cuda-8.0/targets/x86_64-linux/lib/stubs/libcuda.so ..
-
+cmake -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules/ -DBUILD_TIFF=ON -DCUDA_CUDA_LIBRARY=/usr/local/cuda-8.0/targets/x86_64-linux/lib/stubs/libcuda.so -DENABLE_AVX=ON -DWITH_OPENGL=ON -DWITH_OPENCL=ON -DWITH_IPP=ON -DWITH_TBB=ON -DWITH_EIGEN=ON -DWITH_V4L=ON -DWITH_QT=ON -DFORCE_VTK=ON -DWITH_CUBLAS=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DCUDA_NVCC_FLAGS="-D_FORCE_INLINES" -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=$(python3 -c "import sys; print(sys.prefix)") -DPYTHON_EXECUTABLE=$(which python3) -DPYTHON_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -DPYTHON_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") ..     
 make -j $(($(nproc) + 1))
 make install
 /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
